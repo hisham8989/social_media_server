54b1777e82926657d753876d84b773d2ed9f2cdc
Hisham Khan <hisham.khan@trica.co>
2023-03-15 20:06:14 +0530

    change storage path

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/assets");
+    cb(null, path.join(__dirname, "public/assets"));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
