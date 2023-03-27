2098bf22192b9f6a0fb3437d88cf3d15d70eded4
Hisham Khan <hisham.khan@trica.co>
2023-03-28 02:37:44 +0530

    built system locally

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json());
 app.use(helmet());
+app.use(cors());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 if ((env.name = "development")) app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /** File Storage */
